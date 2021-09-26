5299f3e72961043780c4de9c459e802438d80581
Jeff Lynch <jlynch3@uw.edu>
2021-09-25 20:26:53 -0700

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,10 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
-  "/index.html",
-  "/manifest.webmanifest",
-  "/public/style.css",
-  "/assets/js/loadImages.js",
+  "/public/icons/index.html",
+  "/public/icons/manifest.webmanifest",
+  "/public/icons/style.css",
+  "/public/icons/styles.css",
   "public/icons/icon-192x192.png",
   "public/icons/icon-512x512.png",
 
