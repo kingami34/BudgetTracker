37ba5f6811dd23b4d0fc3361b917d542ca593cb4
Jeff Lynch <jlynch3@uw.edu>
2021-09-26 07:52:41 -0700

    first commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,7 @@ const FILES_TO_CACHE = [
   "/public/style.css",
   "/public/icons/icon-192x192.png",
   "/public/icons/icon-512x512.png",
-
+  "/manifest.webmanifest"
 ];
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
